72cd579197a236ae24bebbd4df70ba8d3ba337ec
Doug Aaser <douglasaaser@gmail.com>
2020-05-25 18:29:38 -0400

    Add wrap around
    In the original version of this extension, when you tried to move right
    on the final tab, it would create a tab. I prefer that the former action
    cycles back to the beginning of the tab list, same with when you try to
    move left on the first tab. This also has some small other changes.

--- background.js
+++ background.js
@@ -11,30 +11,37 @@ browser.commands.onCommand.addListener(function(command) {
     }
 
     // find tab that is currently open
-    var tabQuery = browser.tabs.query({currentWindow: true, active: true});
-    tabQuery.then(findOtherTab, onError);
+    browser.tabs.query({currentWindow: true, active: true})
+        .then(findNextTab, onError);
 })
 
-function findOtherTab(currentTabs) {
-    // find all other tabs
-    let otherTab = browser.tabs.query({index: currentTabs[0].index - direction});
-    otherTab.then(switchTab, onError);
+function findNextTab(currentTab) {
+    // get the index of the next tab according to user input
+    console.log(currentTab[0]);
+    var nextIndex = currentTab[0].index + direction;
+
+    browser.tabs.query({currentWindow: true})
+        .then(allTabs => {
+            // check if we're on the first or last tab
+            // if we are, we wanna wrap around to the other extreme
+            if (nextIndex < 0) {
+                nextIndex = allTabs[allTabs.length - 1].index;
+            } else if (nextIndex >= allTabs.length) {
+                nextIndex = 0;
+            }
+
+            // switch to the index of the next tab
+            browser.tabs.query({currentWindow: true, index: nextIndex})
+                .then(switchTab, onError);
+        }, onError);
 }
 
-function switchTab(otherTabs) {
-    console.log(otherTabs.length);
-    if (otherTabs.length) {
-        browser.tabs.update(otherTabs[0].id, {active: true});
-    } else {
-        var creating = browser.tabs.create({});
-        creating.then(onNewTabCreated, onError);
+function switchTab(nextTab) {
+    if (nextTab.length) {
+        browser.tabs.update(nextTab[0].id, {active: true});
     }
 }
 
 function onError(msg) {
     console.log(msg);
-}
-
-function onNewTabCreated(tab) {
-    console.log(tab.id);
 }
